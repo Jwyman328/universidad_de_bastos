952aa5fbdafcbc37e42b41c76803206f236e7107
Joseph Wyman <jwymandev@gmail.com>
2020-08-29 17:07:39 -0400

    Add sorting drow downs to book center

--- src/app/pages/book-center/book-center.component.ts
+++ src/app/pages/book-center/book-center.component.ts
@@ -8,13 +8,60 @@ import { BooksService } from '../../services/http-requests/books.service';
   styleUrls: ['./book-center.component.scss']
 })
 export class BookCenterComponent implements OnInit {
-  bookData;;
+  bookData;
+  selectedSort = 'read';
+  sortedBooks;
+  readStatusOptions = [{displayName:'read', value:'read'}, {displayName:'not read', value:'not read'}]
+  dateOptions = [{displayName:'newest', value:'newest'}, {displayName:'oldest', value:'oldest'}]
+
   constructor(private booksService:BooksService) { }
 
   ngOnInit(): void {
     this.booksService.getBooks().subscribe((res)=>{
       this.bookData = res
     })
+  }
+
+  sortByReadStatus(event){
+    console.log('read status', event)
+    console.log(this.bookData, )
+    this.sortBooksByReadStatus(event.value)
+  }
+
+  sortBooksByReadStatus(readStatus) {
+    let readBooks = [];
+    let unreadBooks = [];
+    const bookDataSorted = this.bookData.map((bookData)=>{
+      if (bookData.hasBeenReadByUser === true){
+        readBooks.push(bookData)
+      }else{
+        unreadBooks.push(bookData)
+      }
+    })
+
+    let allBooksSofted = []
+    if (readStatus === 'read'){
+      console.log(readBooks, 'red books')
+      allBooksSofted = [...readBooks,...unreadBooks]
+    }else{
+      allBooksSofted = [...unreadBooks, ...readBooks]
+    }
+    
+    this.bookData = allBooksSofted
+  }
+
+  sortByDate(sortType){
+    let softTypeValue = sortType;
+    if (sortType.value) {
+      softTypeValue = sortType.value;
+    }
+    this.selectedSort = softTypeValue;
+    const booksSortedByDate = [...this.bookData];
+    booksSortedByDate.sort((a, b) => a.year - b.year);
+    if (softTypeValue === 'newest') {
+      booksSortedByDate.reverse();
+    }
+    this.bookData = booksSortedByDate;
   }
 
 }
