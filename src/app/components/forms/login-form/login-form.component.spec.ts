c58e4f563abb0116b4ac09425f0173a0e99bf43a
Joseph Wyman <jwymandev@gmail.com>
2020-09-05 12:41:41 -0400

    Add login form tests

--- src/app/components/forms/login-form/login-form.component.spec.ts
+++ src/app/components/forms/login-form/login-form.component.spec.ts
@@ -1,40 +1,76 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { LoginFormComponent } from './login-form.component';
-// import { NO_ERRORS_SCHEMA } from '@angular/core';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { UserAuthDataService } from '../../../services/userData/user-auth-data.service';
+import { LoginFormComponent } from './login-form.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UserAuthDataService } from 'src/app/services/http-requests/userData/user-auth-data.service';
+import { Router } from '@angular/router';
+import { LoginUserService } from 'src/app/services/http-requests/login-user.service';
+import { of } from 'rxjs';
+//let handleLoginSubmitSpy: {jasmine.Spy};
+let testBedService;
 
-// //let handleLoginSubmitSpy: {jasmine.Spy};
-// let testBedService;
+fdescribe('LoginFormComponent', () => {
+  let component: LoginFormComponent;
+  let fixture: ComponentFixture<LoginFormComponent>;
+  let router: Router;
+  let userAuthDataService: UserAuthDataService;
+  let loginUserService: LoginUserService;
+  let postLoginUserResponse
 
-// describe('LoginFormComponent', () => {
-//   let component: LoginFormComponent;
-//   let fixture: ComponentFixture<LoginFormComponent>;
+  beforeEach(async(() => {
+    userAuthDataService = jasmine.createSpyObj('UserAuthDataService', [
+      'setToken',
+      'setUsername',
+    ]);
+    loginUserService = jasmine.createSpyObj('LoginUserService',['postLoginUser','handleRequestSuccess','handleRequestError'])
+    postLoginUserResponse = {token:'mockToken', username:'mockUsername'}
+    loginUserService.postLoginUser = jasmine.createSpy('loginUserService').and.returnValue(of(postLoginUserResponse))
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [LoginFormComponent],
+      providers: [
+        { provide: UserAuthDataService, useValue: userAuthDataService },
+        { provide: LoginUserService, useValue: loginUserService },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+  }));
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       imports: [RouterTestingModule],
-// declarations: [ LoginFormComponent ],
-//       providers: [UserAuthDataService],
-//       schemas: [
-//         NO_ERRORS_SCHEMA
-//       ],
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(() => {
+    //handleLoginSubmitSpy = jasmine.createSpyObj(<any>'handleLoginSubmit')
+    testBedService = TestBed.get(UserAuthDataService);
+    fixture = TestBed.createComponent(LoginFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   beforeEach(() => {
-//     //handleLoginSubmitSpy = jasmine.createSpyObj(<any>'handleLoginSubmit')
-//      testBedService = TestBed.get(UserAuthDataService) 
-//     fixture = TestBed.createComponent(LoginFormComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  it('should call postLoginUser when loginform is valid', () => {
+    spyOnProperty(component.loginForm, 'valid').and.returnValue(true)
 
-//   it('should create', () => {
-//     const element:HTMLElement = fixture.debugElement.nativeElement
+    component.handleLoginSubmit()
+    expect(loginUserService.postLoginUser).toHaveBeenCalled()
+  });
 
-//     expect(element.querySelectorAll('input')[0].textContent).toBe('');
-//   });
-// });
+  it('should call authUserService setToken with token if postLoginUser is successful', () => {
+    spyOnProperty(component.loginForm, 'valid').and.returnValue(true)
+    
+    component.handleLoginSubmit()
+    expect(userAuthDataService.setToken).toHaveBeenCalledWith(postLoginUserResponse.token)
+  });
+
+  it('should call authUserService setUsername  if postLoginUser is successful', () => {
+    spyOnProperty(component.loginForm, 'valid').and.returnValue(true)
+    
+    component.handleLoginSubmit()
+    expect(userAuthDataService.setUsername).toHaveBeenCalled()
+  });
+
+  it('should call handleRequestSuccess   if postLoginUser is successful', () => {
+    spyOnProperty(component.loginForm, 'valid').and.returnValue(true)
+    
+    component.handleLoginSubmit()
+    expect(loginUserService.handleRequestSuccess).toHaveBeenCalled()
+  });
+
+});
