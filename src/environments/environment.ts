f9912cc48dd2ca5e658b467a23f883b0f61fcb68
Joseph Wyman <jwymandev@gmail.com>
2020-09-07 10:15:01 -0400

    new docker image

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,8 @@ export const environment = {
   backendAPIBaseUrl: 'http://localhost:5000',
 };
 
+
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
