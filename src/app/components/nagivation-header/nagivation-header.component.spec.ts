f85f18782c2b0b77dd6406e9d1b15383578a0e17
Joseph Wyman <jwymandev@gmail.com>
2020-09-07 11:40:34 -0400

    Add secondary navigation header

--- src/app/components/nagivation-header/nagivation-header.component.spec.ts
+++ src/app/components/nagivation-header/nagivation-header.component.spec.ts
@@ -1,25 +1,30 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { NagivationHeaderComponent } from './nagivation-header.component';
+import { NagivationHeaderComponent } from './nagivation-header.component';
+import { Router } from '@angular/router';
 
-// describe('NagivationHeaderComponent', () => {
-//   let component: NagivationHeaderComponent;
-//   let fixture: ComponentFixture<NagivationHeaderComponent>;
+let routerSpy;
+describe('NagivationHeaderComponent', () => {
+  let component: NagivationHeaderComponent;
+  let fixture: ComponentFixture<NagivationHeaderComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ NagivationHeaderComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    routerSpy = jasmine.createSpyObj('Router',['navigate'])
+    TestBed.configureTestingModule({
+      declarations: [ NagivationHeaderComponent ],
+      providers: [{ provide: Router, useValue: routerSpy }],
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(NagivationHeaderComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NagivationHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('navigateTo should navigate to passed in page ', () => {
+    component.navigateTo('mockPage')
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['mockPage'])
+  });
+});
