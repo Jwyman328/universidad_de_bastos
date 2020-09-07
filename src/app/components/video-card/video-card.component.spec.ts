9588cde10023cd21954c07acb6cd84e8528e123a
Joseph Wyman <jwymandev@gmail.com>
2020-09-07 14:50:24 -0400

    Add video note totaling to video note card

--- src/app/components/video-card/video-card.component.spec.ts
+++ src/app/components/video-card/video-card.component.spec.ts
@@ -2,15 +2,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { VideoCardComponent } from './video-card.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { videoDataMock } from 'src/app/testing/mockData/videoDataMock';
+import { NotesService } from 'src/app/services/http-requests/notes.service';
+import { of } from 'rxjs';
+import { noteMock } from 'src/app/testing/mockData/noteMock';
 
 describe('VideoCardComponent', () => {
   let component: VideoCardComponent;
   let fixture: ComponentFixture<VideoCardComponent>;
-
+  let notesService:NotesService;
   beforeEach(async(() => {
+    notesService = jasmine.createSpyObj('NotesService',['getAllNotesForVideo']);
+    notesService.getAllNotesForVideo = jasmine.createSpy('getAllNotesForVideo').and.returnValue(of([noteMock]))
     TestBed.configureTestingModule({
     imports:[RouterTestingModule],
-      declarations: [ VideoCardComponent ]
+      declarations: [ VideoCardComponent ],
+      providers: [{provide:NotesService, useValue:notesService}]
     })
     .compileComponents();
   }));
@@ -27,4 +33,13 @@ describe('VideoCardComponent', () => {
     component.goToNoteCenter()
     expect(component.route.navigate).toHaveBeenCalledWith(['video-center', videoDataMock.videoUrl,videoDataMock.title])
   });
+
+  it('on ngOnInit should call getAllNotesForVideo with videoUrl', () => {
+    component.videoData = videoDataMock
+    component.ngOnInit()
+    expect(notesService.getAllNotesForVideo).toHaveBeenCalledWith(videoDataMock.videoUrl)
+  });
+
+
+
 });
--- src/app/components/video-card/video-card.component.ts
+++ src/app/components/video-card/video-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { video } from 'src/app/models/video';
+import { NotesService } from 'src/app/services/http-requests/notes.service';
 
 @Component({
   selector: 'app-video-card',
@@ -8,11 +9,16 @@ import { video } from 'src/app/models/video';
   styleUrls: ['./video-card.component.scss']
 })
 export class VideoCardComponent implements OnInit {
+  quantityOfNotes = 0;
   @Input('videoData') videoData:video;
-  constructor(public route: Router) {    
+  constructor(public route: Router, private notesService:NotesService) {    
   }
 
   ngOnInit(): void {
+    const videoId = this.videoData.videoUrl
+    this.notesService.getAllNotesForVideo(videoId).subscribe((allVideoNotes)=>{
+      this.quantityOfNotes = allVideoNotes.length
+    })
   }
 
   goToNoteCenter(){
