9a054a2f5135236122a02cf3e699ce78fbe7f06d
Joseph Wyman <jwymandev@gmail.com>
2020-09-13 08:20:44 -0400

    Add mobile main header

--- src/app/components/nagivation-header/nagivation-header.component.ts
+++ src/app/components/nagivation-header/nagivation-header.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nagivation-header',
   templateUrl: './nagivation-header.component.html',
-  styleUrls: ['./nagivation-header.component.scss']
+  styleUrls: ['./nagivation-header.component.scss'],
 })
 export class NagivationHeaderComponent implements OnInit {
+  isMobileHeader = false;
+  isMobileHeaderOpen = true;
 
-  constructor(private route:Router) { }
+  constructor(private route: Router) {}
+
+  public innerWidth: any;
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    this.onResize(true)
   }
 
-  navigateTo(page){
-    this.route.navigate([page])
-
+  navigateTo(page) {
+    this.route.navigate([page]);
   }
+  @HostListener('window:resize', ['$event'])
 
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth  <= 830){
+      this.isMobileHeader=true
+    }else{
+      this.isMobileHeader=false
+
+    }
+  }
 }
